0e365012c7f8e7ea32e1c6b6a4aea6ec714b33e7
matpanel <matpanel@outlook.com>
2019-10-30 22:19:44 +0100

    PRINTERCLIENT EDITED IN ORDER TO ACCEPT ARGUMENTS FROM CONSOLE

--- src/main/java/gr/matamis/client/PrinterClient.java
+++ src/main/java/gr/matamis/client/PrinterClient.java
@@ -1,5 +1,6 @@
 package gr.matamis.client;
 
+import java.io.Console;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -7,6 +8,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import gr.matamis.server.AuthenticationException;
 import gr.matamis.server.Credentials;
@@ -18,15 +20,59 @@ public class PrinterClient {
 
 
         try {
+/*
+//      Someone can input all arguments at the same time
             String filename = args[0];
             String printerName = args[1];
             String username = args[2];
             String password = args[3];
+ */
+
+/*
+//  someone can input the arguments from the console
+//  sadly this way it cannot be run from inside the IDE.
+
+            Console console = System.console();
+            if (console==null){
+                System.out.println("No console: not in interactive mode!");
+                System.exit(0);
+            }
+            System.out.print("Enter your username: ");
+            String username = console.readLine();
+
+            System.out.println("Enter your password: ");
+            char[] passwordchar = console.readPassword();
+            String password = String.valueOf(passwordchar);
+
+            System.out.print("Which file would you like to print? Enter filename: ");
+            String filename=console.readLine();
+
+            System.out.print("Enter printer name: (hint printer0 or printer1?)");
+            String printerName = console.readLine();
+
+ */
+
+//      Taking input using the Scanner Class
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Enter your username: ");
+            String username = scn.nextLine();
+
+            System.out.println("Enter your password: ");
+            String password = scn.nextLine();
+
+            System.out.println("Which file would you like to print? Enter filename: ");
+            String filename = scn.nextLine();
+
+            System.out.println("Enter printer name: (hint printer0 or printer1)");
+            String printerName = scn.nextLine();
+
             Remote remoteComponent = Naming.lookup("rmi://localhost:5050/printer");
 
             PrintServer printServer = (PrintServer) remoteComponent;
             Credentials panosCredentials = new Credentials(username, password);
-//            Credentials jonasCredentials = new Credentials("jonas", "DTU#02239dtu");
+
+            // the line below was written for testing purposes
+            //            Credentials jonasCredentials = new Credentials("jonas", "DTU#02239dtu");
 
             List<Credentials> credentialsList = new ArrayList<>();
             credentialsList.add(panosCredentials);
